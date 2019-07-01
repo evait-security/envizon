4f47aacf89747c72963f2a61dde676e9c119cf2b
evs-ch <evs-ch@users.noreply.github.com>
2019-07-01 14:12:05 +0200

    only use global selenium options/caps

--- config/initializers/selenium.rb
+++ config/initializers/selenium.rb
@@ -1,17 +1,3 @@
-# config.chrome_args = [
-# '--headless',
-# '--hide-scrollbars',
-# '--ignore-certificate-errors',
-# '--disable-gpu',
-# # '--no-sandbox',
-# '--disable-popup-blocking',
-# '--disable-translate',
-# "--window-size=#{config.width},#{config.height}",
-# '--enable-font-antialiasing',
-# '--font-cache-shared-handle[6]'
-# ]
-# end
-caps = Selenium::WebDriver::Remote::Capabilities.chrome
-caps['javascriptEnabled'] = true
-caps['acceptSslCerts'] = true
-SELENIUM = Selenium::WebDriver.for :remote, url: 'http://localhost:4444/wd/hub', desired_capabilities: caps
+SELENIUM_CAPS = Selenium::WebDriver::Remote::Capabilities.chrome
+SELENIUM_CAPS['javascriptEnabled'] = true
+SELENIUM_CAPS['acceptSslCerts'] = true
