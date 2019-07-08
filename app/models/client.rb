1941619acfa131690411d41c1d47ed32489855cd
Anton Strilez <as@evait.de>
2019-07-08 20:53:17 +0200

    add db helper for images

--- app/models/client.rb
+++ app/models/client.rb
@@ -3,4 +3,8 @@ class Client < ApplicationRecord
   has_many :outputs, dependent: :destroy
   has_and_belongs_to_many :groups
   has_and_belongs_to_many :labels
+  
+  def images
+    ports.joins(:image_attachment).map{|p| p.image}
+  end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,8 @@
 class Group < ApplicationRecord
   validates :name, presence: true
   has_and_belongs_to_many :clients, -> { distinct }
+  
+  def images
+    clients.map{|c| c.images}.flatten
+  end
 end
