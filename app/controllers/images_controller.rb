367f19dc5a7bf098a3a66126539924cd8d06c7e4
Paul Nittmann <pn@evait.de>
2019-07-08 21:21:28 +0200

    add chips to image

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,5 +1,5 @@
 class ImagesController < ApplicationController
     def index
-        @ports_with_images = Port.joins(:image_attachment)
+        @images = Port.joins(:image_attachment).map{|p| p.image}.sort{|i| i.created_at}.reverse
     end
 end
