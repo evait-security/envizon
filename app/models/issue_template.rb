ebd73e6f17358472af83d4a1855d00cc5379975e
Paul Nittmann <pn@evait.de>
2019-10-19 19:47:31 +0200

    move file upload location of scans to shared volume

--- app/models/issue_template.rb
+++ app/models/issue_template.rb
@@ -1,2 +1,18 @@
 class IssueTemplate < ApplicationRecord
+    def color
+        case severity
+        when 0..999
+          return "green"
+        when 1000..1999
+            return "blue"
+        when 2000..2999
+            return "orange"
+        when 3000..3999
+            return "red"
+        when 4000..4999
+            return "purple"
+        else
+            return "grey"
+        end  
+    end
 end
