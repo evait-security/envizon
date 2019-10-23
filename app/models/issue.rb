e330ab89ca95304a0650fd86ff9e89a1652362eb
Anton Strilez <as@evait.de>
2019-10-23 11:29:27 +0200

    add wrapper to create issue from template

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,3 +1,13 @@
 class Issue < ReportPart
   belongs_to :reportable, polymorphic: true
+
+  def self.create_from_template(issueGroup, issueTemplate)
+    Issue.create(
+      reportable: issueGroup, 
+      title: issueTemplate.title, 
+      description: issueTemplate.description, 
+      rating: issueTemplate.rating, 
+      recommendation: issueTemplate.recommendation,
+      severity: issueTemplate.severity ) 
+  end
 end
