244752dcaf230922f6864c3add0f4e11a54c13c8
Paul Nittmann <pn@evait.de>
2019-10-28 23:58:18 +0100

    add port entropy, add global loader, fix some ui

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -10,4 +10,21 @@ class Issue < ReportPart
       recommendation: issueTemplate.recommendation,
       severity: issueTemplate.severity ) 
   end
+  
+  def color
+    case severity
+    when 0..999
+      return "green"
+    when 1000..1999
+        return "blue"
+    when 2000..2999
+        return "orange"
+    when 3000..3999
+        return "red"
+    when 4000..4999
+        return "purple"
+    else
+        return "grey"
+    end  
+  end
 end
