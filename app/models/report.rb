e1a2c4fa0bf866328e9f95be4bf18db539364a9a
Paul Werther <pw@evait.de>
2019-12-04 11:25:43 +0100

    add recursive issues_all func

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,3 +1,24 @@
 class Report < ApplicationRecord
   has_many :report_parts, as: :reportable
+
+  def all_issues
+    issue_array = []
+    report_parts.each do |pt|
+      pt.get_child_issues.each do |child|
+        issue_array << child if child.type == "Issue"
+      end
+    end
+    return issue_array
+  end
+
+
+
+  def all_issues_anton
+    result = []
+    report_parts.each do |child|
+      result << child if child.class == "Issue"
+      result << child.issue if child.class == "IssueGroup"
+    end
+    result.flatten
+  end
 end
--- app/models/report_part.rb
+++ app/models/report_part.rb
@@ -3,4 +3,21 @@ class ReportPart < ApplicationRecord
   has_many :screenshots, dependent: :destroy
   has_many :client_report_parts, foreign_key: :report_part_id
   has_many :clients, through: :client_report_parts
+
+
+  def get_child_issues
+    parts = []
+    if self.type == "Issue"
+      parts << self
+    else
+      report_parts.each do |pt|
+        if pt.type == "Issue"
+          parts << pt
+        else
+        pt.get_child_issues
+        end
+      end
+    end
+    return parts
+  end
 end
