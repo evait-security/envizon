aca9a7eb97007dcced12647de257acc74e947a41
evs-ch <evs-ch@users.noreply.github.com>
2020-03-09 15:31:16 +0100

    use example template.docx file if none present

--- docker/envizon_dev/entrypoint_development.sh
+++ docker/envizon_dev/entrypoint_development.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+
+if [[ ! -f report_templates/envizon_template.docx ]]; then
+  mkdir -p report_templates
+  cp report_templates/envizon_template.docx.example report_templates/envizon_template.docx
+fi
+
 rm -f /usr/src/app/envizon/tmp/pids/server.pid
 bundle exec rails db:migrate
 bundle exec rails db:seed
