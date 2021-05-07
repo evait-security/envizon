47604119f69503b27a6d553afe86e4721af62f84
Paul Werther <pw@evait.de>
2021-05-07 10:48:30 +0200

    remove material color mapping

--- app/models/issue_template.rb
+++ app/models/issue_template.rb
@@ -1,22 +1,6 @@
 class IssueTemplate < ApplicationRecord
   has_many :issues
 
-  def color
-    case severity
-    when 0
-      'green'
-    when 1
-      'blue'
-    when 2
-      'orange'
-    when 3
-      'red'
-    when 4
-      'purple'
-    else
-      'grey'
-    end
-  end
   def color_hex
     case severity
     when 0
