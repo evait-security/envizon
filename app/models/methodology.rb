1f7d0c9144905654d944e03b3bd763ce8b5859bd
myztique <as@evait.de>
2022-05-10 09:43:35 +0200

    fix: placeholder replacement order

--- app/models/methodology.rb
+++ app/models/methodology.rb
@@ -6,7 +6,9 @@ class Methodology < ApplicationRecord
 
   def content_with_placeholders(placeholder_set_id)
     result = content
-    PlaceholderSet.find_by(id: placeholder_set_id)&.placeholder_values&.each do |placeholder_value|
+    PlaceholderSet.find_by(id: placeholder_set_id)&.placeholder_values
+                                                  &.sort_by{|placeholder_value| placeholder_value.placeholder.name.length}.reverse
+                                                  &.each do |placeholder_value|
       result&.gsub!("@@#{placeholder_value.placeholder.name}", "#{placeholder_value.content}")
     end
     result
